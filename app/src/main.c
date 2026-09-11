3efb7407d8909a601b640234e285070887bad4fa
D.M. <grmz9hhf9j@privaterelay.appleid.com>
2026-09-11 18:13:20 +0200

    Doing fetching via work instead of single run test

--- app/src/main.c
+++ app/src/main.c
@@ -6,31 +6,58 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/sys/printk.h>
 
-int main(void)
+#define APP_SAMPLE_RATE_HZ 100U
+#define APP_SAMPLE_PERIOD K_USEC(1000000U / APP_SAMPLE_RATE_HZ)
+
+const struct device *sensor = DEVICE_DT_GET(DT_ALIAS(accel0));
+
+static void sensor_work_handler(struct k_work *work)
 {
-	const struct device *sensor = DEVICE_DT_GET(DT_ALIAS(accel0));
 	struct sensor_value axes[3];
+	int64_t timestamp_ms;
 	int ret;
 
-	if (!device_is_ready(sensor)) {
+	ARG_UNUSED(work);
+
+	ret = sensor_sample_fetch(sensor);
+	if (ret != 0)
+	{
+		printk("Fetch failed: %d\n", ret);
+	}
+
+	timestamp_ms = k_uptime_get();
+
+	ret = sensor_channel_get(sensor, SENSOR_CHAN_ACCEL_XYZ, axes);
+	if (ret != 0)
+	{
+		printk("Get failed %d\n", ret);
+		return;
+	}
+	printk("t=%lld ms, X=%lld, Y=%lld, Z=%lld micro m/s^2\n",
+		   (long long)timestamp_ms,
+		   (long long)sensor_value_to_micro(&axes[0]),
+		   (long long)sensor_value_to_micro(&axes[1]),
+		   (long long)sensor_value_to_micro(&axes[2]));
+}
+K_WORK_DEFINE(sensor_work, sensor_work_handler);
+
+static void sample_timer_handler(struct k_timer *timer)
+{
+	ARG_UNUSED(timer);
+
+	k_work_submit(&sensor_work);
+}
+K_TIMER_DEFINE(sample_timer, sample_timer_handler, NULL);
+
+int main(void)
+{
+	if (!device_is_ready(sensor))
+	{
 		printk("Sensor not ready\n");
 		return 1;
 	}
 
-	ret = sensor_sample_fetch(sensor);
-	if (ret != 0) {
-		printk("Fetch failed: %d\n", ret);
-		return 1;
-	}
-
-	ret = sensor_channel_get(sensor, SENSOR_CHAN_ACCEL_XYZ, axes);
-	if (ret != 0) {
-		printk("Get failed: %d\n", ret);
-		return 1;
-	}
-
-	printk("PASS: %s, Z = %d micro m/s^2\n",
-	       sensor->name, axes[2].val1 * 1000000 + axes[2].val2);
+	k_timer_start(&sample_timer, APP_SAMPLE_PERIOD, APP_SAMPLE_PERIOD);
 
 	return 0;
 }
